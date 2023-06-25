ba3ef59f5d23c112d945040ec1e127eeb5ddb6b9
sergio-egm <s.e.g.manfrin@gmail.com>
2023-06-25 11:31:42 +0200

    Updateing libraries

---
+++ lib/BlockAnalisys/BlockAnalisys.cpp
@@ -0,0 +1,62 @@
+/****************************************************************
+*****************************************************************
+
+Sergio Eligio Giovanni Manfrin
+Matricola 968093
+
+Laboratorio di Simulazione Numerica
+
+Classes usefull for block average analisys
+
+*****************************************************************
+****************************************************************/
+
+#include "BlockAnalisys.h"
+
+
+BlockAnalisys::BlockAnalisys(
+		unsigned int in_M,
+	       	unsigned int in_N)
+	:M{in_M} , N{in_N}
+{
+	L=M/N;
+	//Check if M is a multiple of N
+	if (M%N!=0){
+		std::cerr<<"WARNING: ";
+		std::cerr<<"The number of samples is not a multiple of block number"<<std::endl;
+	}
+}
+
+//Run the simulation
+void BlockAnalisys::Run(Method& simul){
+	for (unsigned int i=0; i<N ;i++){
+
+		//Uncomment these lines to see box counting
+		//std::cout<<std::endl<<"----------------------"<<std::endl;
+		//std::cout<<std::endl<<"Block number "<<i+1<<std::endl;
+
+		for (unsigned int j=0; j<L ;j++){
+			simul.Run();
+			simul.Accumulate();
+		}
+		simul.Reset();
+	}
+
+	//std::cout<<std::endl<<"----------------------"<<std::endl;
+}
+
+
+
+
+
+
+/****************************************************************
+*****************************************************************
+
+Sergio Eligio Giovanni Manfrin
+Matricola 968093
+
+Laboratorio di Simulazione Numerica
+
+*****************************************************************
+****************************************************************/
--- setup.sh
+++ setup.sh
@@ -1,2 +1,19 @@
-export CPLUS_INCLUDE_PATH=$PWD/random_lsn:$CPLUS_INCLUDE_PATH
-export LIBRARY_PATH=$PWD/random_lsn:$LIBRARY_PATH
+echo Setup start...
+echo
+
+echo Setup of random library:
+export CPLUS_INCLUDE_PATH=$PWD/lib/random:$CPLUS_INCLUDE_PATH
+echo - Included path for c++ code
+export LIBRARY_PATH=$PWD/lib/random:$LIBRARY_PATH
+echo - Included path for c++ compiler
+
+echo 
+
+echo Setup of BlockAnalisys library:
+export CPLUS_INCLUDE_PATH=$PWD/lib/BlockAnalisys:$CPLUS_INCLUDE_PATH
+echo - Included path for c++ code
+export LIBRARY_PATH=$PWD/lib/BlockAnalisys:$LIBRARY_PATH
+echo - Included path for c++ compiler
+
+echo 
+echo Setup done!
